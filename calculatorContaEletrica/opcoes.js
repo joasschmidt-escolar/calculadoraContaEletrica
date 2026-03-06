cd9c05125430f7bcf951152bab41901901bb127d
joasschmidt-escolar <joas_schmidt@estudante.sesisenai.org.br>
2026-03-06 18:31:34 -0300

    Add files via upload

---
+++ calculatorContaEletrica/opcoes.js
@@ -0,0 +1,19 @@
+function opcoes(){
+    let item = parseInt(prompt("Digite [1] para geladeira\n[2] para Tv\n[3] para ar condiconado\n[4] para chuveiro elétrico"));
+    let potenciaWatt;
+
+    switch(item){
+        case 1: 
+            potenciaWatt = 800;
+            break;
+        case 2:
+            potenciaWatt  = 35;
+            break;
+        case 3:
+            potenciaWatt = 1200;
+            break;
+        case 4:
+            potenciaWatt = 800;
+            break;
+    }
+}
