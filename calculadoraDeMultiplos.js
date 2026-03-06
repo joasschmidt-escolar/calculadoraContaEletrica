a4d59e9f3abee99b8d85412621c1ffd5558af05a
joasschmidt-escolar <joas_schmidt@estudante.sesisenai.org.br>
2026-03-06 19:26:03 -0300

    Versão 1.1.1
    versão com bugs🪲

---
+++ calculadoraDeMultiplos.js
@@ -0,0 +1,20 @@
+/*calculadora de
+multiplos e submultiplos
+
+
+- input > unidade(A,I, ohm, W,V)
+- input > valor numerico (450)
+- input > qual conversão?
+conversão > da unidada principal >>> mili*/
+
+/*while(continua){
+    let imput = parseInt(prompt("Digite [1] para corrente elétrica\n[2] para tensão elétrica\n[3] para resistência elétrica\n[4] potência elétrica "));
+
+    switch(imput){
+        case 1: 
+            correnteEletrica();
+            break;
+        case 2:
+            //Em desenvolvimento
+    }
+}*/
---
+++ correnteEletrica.js
@@ -0,0 +1,4 @@
+/*function correnteEletrica(){
+    //Em desenvolvimento
+    let uni = parseInt(prompt("Digite [1] para mA --> A\n[2] para µA --> A"))
+}*/
